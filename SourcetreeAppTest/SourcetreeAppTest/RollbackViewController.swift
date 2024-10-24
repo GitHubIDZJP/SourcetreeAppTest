e0b0ff3d69defd72c85c5caa22d236e0a89ab833
mac <zjp15083791743@163.com>
2024-10-24 22:28:44 +0800

    Version: 3 Mark: 回滚 描述： 1 丢弃某个文件，执行回滚操作

---
+++ SourcetreeAppTest/SourcetreeAppTest/RollbackViewController.swift
@@ -0,0 +1,21 @@
+//
+//  RollbackViewController.swift
+//  SourcetreeAppTest
+//
+//  Created by mac on 24.10.24.
+//
+
+import UIKit
+//回滚操作，我假设执行下回滚
+class RollbackViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+   
+
+}
