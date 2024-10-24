9e1c2ba66594831fab7c800f1d879e6e3f7b3989
mac <zjp15083791743@163.com>
2024-10-24 23:33:00 +0800

    no message

--- SourcetreeAppTest/SourcetreeAppTest/DevBranchViewController.swift
+++ SourcetreeAppTest/SourcetreeAppTest/DevBranchViewController.swift
@@ -12,7 +12,7 @@ class DevBranchViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
- 
+
         //新建分之
 //        Xcode新建了个DevBranchViewController类，在用Dev分之commit并Push后，仓库能看到master和dev代码，但是克隆只能克隆master的代码，并不会把dev的代码一并克隆下来
         
