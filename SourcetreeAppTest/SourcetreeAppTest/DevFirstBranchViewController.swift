f25eeaba924594ce1d6e1bbdb4617a997915f247
mac <zjp15083791743@163.com>
2024-10-24 23:50:39 +0800

    12

---
+++ SourcetreeAppTest/SourcetreeAppTest/DevFirstBranchViewController.swift
@@ -0,0 +1,22 @@
+//
+//  DevFirstBranchViewController.swift
+//  SourcetreeAppTest
+//
+//  Created by mac on 24.10.24.
+//
+
+import UIKit
+
+class DevFirstBranchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//分支合并到master ,再让本地master推送到远程master
+//       1 + 1 = 2
+        
+    }
+    
+
+    
+
+}
