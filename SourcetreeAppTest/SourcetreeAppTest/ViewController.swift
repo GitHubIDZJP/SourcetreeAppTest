413835afe2dd1ca3508d37e44d69d23dd6785fed
mac <zjp15083791743@163.com>
2024-10-24 22:24:08 +0800

    Version:   1 Mark: 丢弃文件 描述: 1.  丢弃文件操作会把Xcode本地修改过的类代码”清除“  2. 移除操作会直接移除当前类，Xcode被Sourcetree操作移除的类会报红，只有个路径形式存在Xcode里(慎重操作)

---
+++ SourcetreeAppTest/SourcetreeAppTest/ViewController.swift
@@ -0,0 +1,14 @@
+//  Created by mac on 24.10.24.
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+
+}
+
