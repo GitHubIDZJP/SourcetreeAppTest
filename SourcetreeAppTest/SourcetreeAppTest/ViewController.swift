c40cd80f27b961ddcd7462219a2031a58d0f5d4b
mac <zjp15083791743@163.com>
2024-10-24 22:38:59 +0800

    Version: 5 Mark:  丢弃区块 描述： 1  丢弃区块：也会直接类似回滚，本地工作区(Xcode)代码操作过的会直接消失

--- SourcetreeAppTest/SourcetreeAppTest/ViewController.swift
+++ SourcetreeAppTest/SourcetreeAppTest/ViewController.swift
@@ -7,9 +7,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        回滚操作1 
+        //        回滚操作1
         
-//        如果是某个类，没有被删除后就指向某个提交，然后回滚，那那个类就没了，意思就是回滚到上次操作之前，比如Xcode新建了一个RollbackViewController,执行了回滚，那这个类就没了(慎用)
+        //        如果是某个类，没有被删除后就指向某个提交，然后回滚，那那个类就没了，意思就是回滚到上次操作之前，比如Xcode新建了一个RollbackViewController,执行了回滚，那这个类就没了(慎用)
+        
+    
+//        如果你已经合并了一个分支，但希望撤销该合并，可以通过回滚合并提交的方式撤销。
+        
+//       丢弃区块：也会直接类似回滚，本地工作区(Xcode)代码操作过的会直接消失
     }
 
 
