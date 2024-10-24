7012d5378c936ec76f9e8e61a2d6f8910acd7191
mac <zjp15083791743@163.com>
2024-10-24 22:32:04 +0800

    Version: 4 Mark: 回滚 描述： 1 丢弃某个文件，执行回滚操作  2，如果是某个类，没有被删除后就指向某个提交，然后回滚，那那个类就没了，意思就是回滚到上次操作之前，比如Xcode新建了一个RollbackViewController,执行了回滚，那这个类就没了(慎用)

--- SourcetreeAppTest/SourcetreeAppTest/ViewController.swift
+++ SourcetreeAppTest/SourcetreeAppTest/ViewController.swift
@@ -9,7 +9,7 @@ class ViewController: UIViewController {
         
 //        回滚操作1 
         
-       
+//        如果是某个类，没有被删除后就指向某个提交，然后回滚，那那个类就没了，意思就是回滚到上次操作之前，比如Xcode新建了一个RollbackViewController,执行了回滚，那这个类就没了(慎用)
     }
 
 
